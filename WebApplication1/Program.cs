7d29d22fe3d361767e06af7b238aed31ee5f7c2d
Kaandnz <konmdeniz@gmail.com>
2025-05-13 16:55:44 +0300

    Add project files.

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+using WebApplication8.Models;
+using WebApplication8.Repository;
+using WebApplication8.Settings;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+var mongoDbSettings = builder.Configuration.GetSection("MongoDbSettings").Get<MongoDbSettings>();
+builder.Services.AddSingleton(mongoDbSettings);
+builder.Services.AddSingleton<IMongoClient>(new MongoClient(mongoDbSettings.ConnectionString));
+builder.Services.AddScoped<IIcecekRepository, IcecekRepository>();
+builder.Services.AddScoped<IYemekRepository, YemekRepository>();
+builder.Services.AddScoped<IGarsonRepository, GarsonRepository>();
+builder.Services.AddScoped<IGenericRepository<Garson>, GenericRepository<Garson>>();
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
---
+++ WebApplication1/Repository/GarsonRepository.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Driver;
+using WebApplication8.Models;
+using WebApplication8.Settings;
+
+namespace WebApplication8.Repository
+{
+    public class GarsonRepository : GenericRepository<Garson>, IGarsonRepository
+    {
+        private readonly IMongoCollection<Garson> _garsonCollection;
+
+        public GarsonRepository(IMongoClient mongoClient, MongoDbSettings settings)
+            : base(mongoClient, settings)
+        {
+            var database = mongoClient.GetDatabase(settings.DatabaseName);
+            _garsonCollection = database.GetCollection<Garson>("garsons");
+        }
+
+        public IEnumerable<Garson> GetComplatedGarsons()
+        {
+            return _garsonCollection.Find(g => g.siparisTamamlandi == true).ToList();
+        }
+
+        public IEnumerable<Garson> GetUnfinishedGarsons()
+        {
+            return _garsonCollection.Find(g => g.siparisTamamlandi == false).ToList();
+        }
+    }
+}
